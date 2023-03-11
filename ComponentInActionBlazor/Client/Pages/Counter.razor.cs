3e72b00b4b3ec889e1fdaa2d6db9dacca5520a7b
avemumin <jacogitproject@gmail.com>
2023-03-11 15:31:59 +0100

    Invoke static c# methods from js

--- ComponentInActionBlazor/Client/Pages/Counter.razor.cs
+++ ComponentInActionBlazor/Client/Pages/Counter.razor.cs
@@ -1,10 +1,24 @@
-﻿namespace ComponentInActionBlazor.Client.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ComponentInActionBlazor.Client.Pages;
 public partial class Counter
 {
+    [Inject] IJSRuntime js { get; set; }
+
     private int currentCount = 0;
-    private void IncrementCount()
+    private static int currentCountStatic = 0;
+    private async Task IncrementCount()
     {
         currentCount++;
+        currentCountStatic++;
+        await js.InvokeVoidAsync("dotnetStativInvocation");
+    }
+
+    [JSInvokable]
+    public static Task<int> GetCurrentCount()
+    {
+        return Task.FromResult(currentCountStatic);
     }
 }
 
--- ComponentInActionBlazor/Client/wwwroot/js/Utilities.js
+++ ComponentInActionBlazor/Client/wwwroot/js/Utilities.js
@@ -1,3 +1,10 @@
 function my_function(message) {
-    console.log("From function in Utilities " +message);
+    console.log("From function in Utilities " + message);
+}
+
+function dotnetStativInvocation() {
+    DotNet.invokeMethodAsync("ComponentInActionBlazor.Client", "GetCurrentCount")
+        .then(result => {
+            console.log("count from javascript " + result)
+        });
 }
