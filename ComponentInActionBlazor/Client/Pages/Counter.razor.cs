e8fccccfe9b0301e5ae664730f5cf3a36ec73d52
avemumin <jacogitproject@gmail.com>
2023-03-11 15:13:56 +0100

    Separate c# code from razor ppage on Counter component

---
+++ ComponentInActionBlazor/Client/Pages/Counter.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace ComponentInActionBlazor.Client.Pages;
+public partial class Counter
+{
+    private int currentCount = 0;
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
+
