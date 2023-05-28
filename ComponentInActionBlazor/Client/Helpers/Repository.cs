ddb38c04c5237452ce44b88af2a611cd5ea80bc8
Jacek Szczepański <jacogitproject@gmail.com>
2023-05-28 20:38:43 +0200

    Rebuild project and add property to poster display with included source via poster link

--- ComponentInActionBlazor/Client/Helpers/Repository.cs
+++ ComponentInActionBlazor/Client/Helpers/Repository.cs
@@ -9,9 +9,18 @@ public class Repository : IRepository
         var movies = new List<Movie>();
         if (!movies.Any())
         {
-            movies.Add(new Movie() { Id = 1, Title = "Spider-Man: Far From Home", ReleaseDate = new DateTime(2019, 7, 2) });
-            movies.Add(new Movie() { Id = 2, Title = "Mohana", ReleaseDate = new DateTime(2016, 11, 23) });
-            movies.Add(new Movie() { Id = 3, Title = "Inception", ReleaseDate = new DateTime(2010, 7, 16) });
+            movies.Add(new Movie() { Id = 1, Title = "Spider-Man: Far From Home",
+                ReleaseDate = new DateTime(2019, 7, 2),
+                Poster = "https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UY474_.jpg "
+            });
+            movies.Add(new Movie() { Id = 2, Title = "Moana",
+                ReleaseDate = new DateTime(2016, 11, 23),
+                Poster = "https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_FMjpg_UY474_.jpg "
+            });
+            movies.Add(new Movie() { Id = 3, Title = "Inception",
+                ReleaseDate = new DateTime(2010, 7, 16),
+                Poster = "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UY474_.jpg"
+            });
         }
         return movies;
     }
--- ComponentInActionBlazor/Shared/Data/Movie.cs
+++ ComponentInActionBlazor/Shared/Data/Movie.cs
@@ -5,5 +5,20 @@ public class Movie
     public int Id { get; set; }
     public required string Title { get; set; }
     public DateTime ReleaseDate { get; set; }
+    public string Poster { get; set; }
+
+    public string TitleBrief
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(Title))
+                return null;
+
+            if (Title.Length > 60)
+                return Title.Substring(0, 60) + "...";
+
+            return Title;
+        }
+    }
 }
 
