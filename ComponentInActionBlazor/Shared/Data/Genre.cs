a13639dd38435b1b60e3e2e0d3eab2828c1d2471
Jacek Szczepański <jacogitproject@gmail.com>
2023-06-03 20:25:22 +0200

    simple validation and error message for InputText component

---
+++ ComponentInActionBlazor/Shared/Data/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComponentInActionBlazor.Shared.Data;
+
+public class Genre
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Name is required and cannot be empty")]
+    public string Name { get; set; }
+}
+
