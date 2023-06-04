ce19b084e421f42f90984ffbb508a3ecfbddebcb
Jacek Szczepański <jacogitproject@gmail.com>
2023-06-04 14:57:09 +0200

    Filter and Person form

---
+++ ComponentInActionBlazor/Shared/Data/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComponentInActionBlazor.Shared.Data;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Biography { get; set; }
+    public string Picture { get; set; }
+    [Required]
+    public DateTime? DateOfBirth { get; set; }
+}
+
